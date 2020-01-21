4e156bbce91f8e879dc767ad66b69568419b6bcc
Brad Saund <brad.saund@gmail.com>
2020-01-21 14:19:21 -0500

    Completed first version of publishing as a cubelist

--- mps_shape_completion_visualization/src/mps_shape_completion_visualization/conversions.py
+++ mps_shape_completion_visualization/src/mps_shape_completion_visualization/conversions.py
@@ -5,10 +5,6 @@ from geometry_msgs.msg import Point
 from std_msgs.msg import ColorRGBA
 
 
-def binvox_to_occupancyStamped(bv):
-    """
-    INPUT: bv: DATATYPE: color
-    """
 
 
 
@@ -29,23 +25,31 @@ def occupancyStamped_to_cubelist(occupancy_msg, color):
 
     
     m = Marker()
-    m.header.frame_id = "/base_frame"
-    m.header.stamp = rospy.get_rostime()
+    m.header = occupancy_msg.header
 
+    m.color = color
     m.type = m.CUBE_LIST
     m.action = m.ADD
     m.pose.orientation.w = 1
 
-    m.scale.x = 0.1
-    m.scale.y = 0.1
-    m.scale.z = 0.1
+    scale = occupancy_msg.scale
+    m.scale.x = scale
+    m.scale.y = scale
+    m.scale.z = scale
+    
+    dim = occupancy_msg.occupancy.layout.dim
+    data_offset = occupancy_msg.occupancy.layout.data_offset
 
-    m.color = color
-
-    for i in range(10):
-        p = Point()
-        p.x = float(i)/10
-        p.y = 0
-        p.z = 0
-        m.points.append(p)
+    for i in range(dim[0].size):
+        for j in range(dim[1].size):
+            for k in range(dim[2].size):
+                val = occupancy_msg.occupancy.data[data_offset + dim[1].stride * i + dim[2].stride * j + k]
+                if val < 0.5:
+                    continue
+                p = Point()
+                p.x = scale/2 + i*scale
+                p.y = scale/2 + j*scale
+                p.z = scale/2 + k*scale
+                m.points.append(p)
+                
     return m
