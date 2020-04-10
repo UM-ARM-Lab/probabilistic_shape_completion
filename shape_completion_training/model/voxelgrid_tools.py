333c372da924a40476a451daaf6f37bc25c8b269
Brad Saund <brad.saund@gmail.com>
2020-04-09 20:40:29 -0400

    Added p_correct calculations

--- shape_completion_training/model/voxelgrid_tools.py
+++ shape_completion_training/model/voxelgrid_tools.py
@@ -21,3 +21,27 @@ def iou(voxelgrid_1, voxelgrid_2, threshold=0.5):
     intersection = tf.reduce_sum(tf.cast((v1 + v2) > 1.5, tf.float32))
     union = tf.reduce_sum(tf.cast((v1+v2) > 0.5, tf.float32))
     return intersection / union
+
+
+def p_correct(estimate_voxelgrid, gt_voxelgrid):
+    """
+    Calculates the p(gt_voxelgrid | estimate_voxelgrid), assuming 
+    estimate_voxelgrid contains a probability (0-1) of each voxel being occupied
+    gt_voxelgrid contains the occupancy {0,1.0}
+    """
+    gt = tf.cast(gt_voxelgrid > 0.5, tf.float32)
+    p_voxel_correct = 1.0 - (gt - estimate_voxelgrid)
+    p_correct = tf.exp(tf.reduce_sum(tf.math.log(p_voxel_correct)))
+    return p_correct
+
+def p_correct_geometric_mean(estimate_voxelgrid, gt_voxelgrid):
+    """
+    Calculates the geometric mean p(gt_voxelgrid | estimate_voxelgrid), assuming 
+    estimate_voxelgrid contains a probability (0-1) of each voxel being occupied
+    gt_voxelgrid contains the occupancy {0,1.0}
+    """
+    gt = tf.cast(gt_voxelgrid > 0.5, tf.float32)
+    p_voxel_correct = 1.0 - (gt - estimate_voxelgrid)
+    num_elements = tf.cast(tf.size(p_voxel_correct), tf.float32)
+    p_correct = tf.exp(tf.reduce_sum(tf.math.log(p_voxel_correct))/num_elements)
+    return p_correct
