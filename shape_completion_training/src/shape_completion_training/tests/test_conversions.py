4b1c64f5288285df1d626a6e32f72b527bf6bbfa
Brad Saund <brad.saund@gmail.com>
2020-05-07 21:29:36 -0400

    Add function to apply rigid body transform to voxel grid

--- shape_completion_training/src/shape_completion_training/tests/test_conversions.py
+++ shape_completion_training/src/shape_completion_training/tests/test_conversions.py
@@ -1,22 +1,25 @@
 from unittest import TestCase
 from setup_data_for_unit_tests import load_test_files
-from shape_completion_training.voxelgrid.conversions import voxelgrid_to_pointcloud, pointcloud_to_voxelgrid
+from shape_completion_training.voxelgrid.conversions import voxelgrid_to_pointcloud, pointcloud_to_voxelgrid, \
+    transform_voxelgrid
 import numpy as np
 
 
 class TestConversions(TestCase):
     def test_voxelgrid_to_pointcloud_on_simple(self):
         vg_orig = np.zeros([3, 3, 3])
+        vg_orig[(1, 2), (0, 2), (1, 0)] = 1.0
         pt_cloud = voxelgrid_to_pointcloud(vg_orig)
-        vg_new = pointcloud_to_voxelgrid(pt_cloud, add_leading_dim=True, add_trailing_dim=True, shape=[3, 3, 3])
+        vg_new = pointcloud_to_voxelgrid(pt_cloud, add_leading_dim=False, add_trailing_dim=False, shape=[3, 3, 3])
         self.assertTrue((vg_orig == vg_new).all())
 
     def test_voxelgrid_to_pointcloud_with_scaling_on_simple(self):
         vg_orig = np.zeros([3, 3, 3])
+        vg_orig[(1, 2), (0, 2), (1, 0)] = 1.0
         for scale in [0.02, 1.0, 10., 10000.]:
             pt_cloud = voxelgrid_to_pointcloud(vg_orig, scale=scale)
             vg_new = pointcloud_to_voxelgrid(pt_cloud, scale=scale,
-                                             add_leading_dim=True, add_trailing_dim=True, shape=[3, 3, 3])
+                                             add_leading_dim=False, add_trailing_dim=False, shape=[3, 3, 3])
             self.assertTrue((vg_orig == vg_new).all())
 
     def test_voxelgrid_to_pointcloud_with_scaling(self):
@@ -27,18 +30,42 @@ class TestConversions(TestCase):
                                              add_leading_dim=True, add_trailing_dim=True, shape=(64, 64, 64))
             self.assertTrue((vg_orig == vg_new).all(), "Failed on scaling {}".format(scale))
 
+    def test_voxelgrid_to_pointcloud_with_scaling_and_origin(self):
+        vg_orig = load_test_files()[0]
+        origin = [120, -12, 130]
+        for scale in [0.01, 1.0, 10., 10000.]:
+            pt_cloud = voxelgrid_to_pointcloud(vg_orig, scale=scale, origin=origin)
+            vg_new = pointcloud_to_voxelgrid(pt_cloud, scale=scale, origin=origin,
+                                             add_leading_dim=True, add_trailing_dim=True, shape=(64, 64, 64))
+            self.assertTrue((vg_orig == vg_new).all(), "Failed on scaling {}".format(scale))
+
     def test_voxelgrid_to_pointcloud_with_scaling_and_translation(self):
         vg_orig = load_test_files()[0]
         for scale in [0.01, 1.0, 10., 10000.]:
             pt_cloud = voxelgrid_to_pointcloud(vg_orig, scale=scale)
-            pt_cloud += scale/4
+            pt_cloud += scale / 4
             vg_new = pointcloud_to_voxelgrid(pt_cloud, scale=scale,
                                              add_leading_dim=True, add_trailing_dim=True, shape=(64, 64, 64))
             self.assertTrue((vg_orig == vg_new).all(), "Failed on scaling {}".format(scale))
 
     def test_voxelgrid_to_pointcloud_on_large_data(self):
         d = load_test_files()
-        for vg_orig in d:
+        for i, vg_orig in enumerate(d):
             pt_cloud = voxelgrid_to_pointcloud(vg_orig)
             vg_new = pointcloud_to_voxelgrid(pt_cloud, add_leading_dim=True, add_trailing_dim=True)
-            self.assertTrue((vg_orig == vg_new).all())
+            self.assertTrue((vg_orig == vg_new).all(), "Failed on point cloud {}".format(i))
+
+
+class TestTransforms(TestCase):
+    def test_transform_voxelgrid_on_empty(self):
+        vg_orig = np.zeros([3, 3, 3])
+        T = np.eye(4)
+        vg_new = transform_voxelgrid(vg_orig, T)
+        self.assertTrue((vg_new == vg_orig).all())
+
+    def test_transform_voxelgrid_using_identity_on_simple_data(self):
+        vg_orig = np.zeros([3, 3, 3])
+        vg_orig[(1, 2), (0, 2), (1, 0)] = 1.0
+        T = np.eye(4)
+        vg_new = transform_voxelgrid(vg_orig, T)
+        self.assertTrue((vg_new == vg_orig).all())
--- shape_completion_training/src/shape_completion_training/voxelgrid/conversions.py
+++ shape_completion_training/src/shape_completion_training/voxelgrid/conversions.py
@@ -1,7 +1,24 @@
 import numpy as np
 
 
-def voxelgrid_to_pointcloud(voxelgrid, scale=1.0, origin=[0,0,0], threshold=0.5):
+def strip_extra_dims(vg):
+    """
+    strips the leading and trailing dimensions from the voxelgrid
+    @param vg:
+    @return: stripped voxelgrid, has_leading_dim, has_ending_dim
+    """
+    return np.squeeze(vg), vg.shape[0] == 1, vg.shape[-1] == 1
+
+
+def add_extra_dims(vg, add_leading_dim, add_trailing_dim):
+    if add_leading_dim:
+        vg = np.expand_dims(vg, 0)
+    if add_trailing_dim:
+        vg = np.expand_dims(vg, -1)
+    return vg
+
+
+def voxelgrid_to_pointcloud(voxelgrid, scale=1.0, origin=[0, 0, 0], threshold=0.5):
     """
     Converts a 3D voxelgrid into a 3D set of points for each voxel with value above threshold
     @param voxelgrid: (opt 1 x) X x Y x Z (opt x 1) voxelgrid
@@ -10,12 +27,12 @@ def voxelgrid_to_pointcloud(voxelgrid, scale=1.0, origin=[0,0,0], threshold=0.5)
     @param threshold:
     @return:
     """
-    pts = np.argwhere(np.squeeze(voxelgrid)>threshold)
+    pts = np.argwhere(np.squeeze(voxelgrid) > threshold)
     return (np.array(pts) - origin + 0.5) * scale
 
 
-def pointcloud_to_voxelgrid(pointcloud, scale=1.0, origin=[0,0,0], shape=(64,64,64),
-                            add_leading_dim = False, add_trailing_dim=False):
+def pointcloud_to_voxelgrid(pointcloud, scale=1.0, origin=[0, 0, 0], shape=(64, 64, 64),
+                            add_leading_dim=False, add_trailing_dim=False):
     """
     Converts a set of 3D points into a binary voxel grid
     @param pointcloud:
@@ -27,11 +44,29 @@ def pointcloud_to_voxelgrid(pointcloud, scale=1.0, origin=[0,0,0], shape=(64,64,
     @return:
     """
     vg = np.zeros(shape)
-    s = (pointcloud/scale + origin).astype(int)
-    vg[s[:,0], s[:,1], s[:,2]] = 1.0
+    s = (pointcloud / scale + origin).astype(int)
+    vg[s[:, 0], s[:, 1], s[:, 2]] = 1.0
+    return add_extra_dims(vg, add_leading_dim, add_trailing_dim)
 
-    if add_leading_dim:
-        vg = np.expand_dims(vg, 0)
-    if add_trailing_dim:
-        vg = np.expand_dims(vg, -1)
-    return vg
+
+def transform_voxelgrid(vg, transform, scale=1.0, origin=(0, 0, 0)):
+    """
+
+    @param vg: voxelgrid
+    @param transform: 4x4 homogeneous tramsform matrix
+    @return:
+    """
+    vg, add_leading_dim, add_trailing_dim = strip_extra_dims(vg)
+
+    pt_cloud = voxelgrid_to_pointcloud(vg)
+
+    if pt_cloud.shape[0] == 0:
+        return vg
+
+    R = transform[0:3, 0:3]
+    t = transform[0:3,3]
+    trans_cloud = np.dot(R,pt_cloud.transpose()).transpose() + t
+    return pointcloud_to_voxelgrid(trans_cloud, scale=scale, origin=origin, shape=vg.shape,
+                                   add_trailing_dim=add_trailing_dim,
+                                   add_leading_dim=add_leading_dim)
+
