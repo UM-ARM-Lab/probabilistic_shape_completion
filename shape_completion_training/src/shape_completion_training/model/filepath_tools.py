5b501204c9c8de92e11d8b1cef55fcbcda384dfe
Brad Saund <brad.saund@gmail.com>
2020-05-21 17:17:17 -0400

    Fix filepath types

--- shape_completion_training/src/shape_completion_training/model/filepath_tools.py
+++ shape_completion_training/src/shape_completion_training/model/filepath_tools.py
@@ -19,10 +19,24 @@ def make_unique_trial_subdirectory_name(*names):
 
 
 def create_or_load_trial(group_name=None, trial_path=None, params=None, trials_directory=None, write_summary=True):
+    """
+    @param group_name:
+    @type group_name: str
+    @param trial_path:
+    @type trial_path: str
+    @param params:
+    @type params: dict
+    @param trials_directory:
+    @type trials_directory: str
+    @param write_summary: Only applies when creating a new trial
+    @type write_summary: bool
+    @return: trial_path, trial_hyper_parameters
+    @rtype: (pathlib.Path, dict)
+    """
     if trial_path is not None:
         # Resume and warn for errors
         if group_name is not None:
-            msg = "Ignoring trial_name {} and resuming from trial_path {}".format(group_name, trial_path)
+            msg = "Ignoring group_name {} and resuming from trial_path {}".format(group_name, trial_path)
             print(Fore.YELLOW + msg + Fore.RESET)
         if params is not None:
             print(Fore.YELLOW + "Ignoring params, loading existing ones" + Fore.RESET)
@@ -33,7 +47,16 @@ def create_or_load_trial(group_name=None, trial_path=None, params=None, trials_d
         return create_trial('tmp', params, trials_directory, write_summary)
 
 
-def load_trial(trial_path=None):
+def load_trial(trial_path):
+    """
+    @param trial_path: full path or relative path from shape_completion_training/trials
+    @type trial_path: str
+    @return:
+    """
+    trial_path = pathlib.Path(trial_path)
+    if not trial_path.is_absolute():
+        r = rospkg.RosPack()
+        trial_path = pathlib.Path(r.get_path('shape_completion_training')) / "trials" / trial_path
     if not trial_path.is_dir():
         raise ValueError("Cannot load, the path {} is not an existing directory".format(trial_path))
 
