341e76a351e5ef0f3d6ac34998a5215b2ee01556
Brad Saund <brad.saund@gmail.com>
2020-07-22 12:01:55 -0400

    In evaluate, load approrpiate dataset given model params

--- shape_completion_training/scripts/evaluate.py
+++ shape_completion_training/scripts/evaluate.py
@@ -6,9 +6,12 @@ from shape_completion_training.model.modelrunner import ModelRunner
 from shape_completion_training.utils import data_tools
 import rospy
 
-MODELS_TO_EVALUATE = ["VAE/VAE_trial_1",
+MODELS_TO_EVALUATE = [
+    # "VAE/VAE_trial_1",
     # "Augmented_VAE/May_21_20-00-00_0000000000",
-    "NormalizingAE/July_02_15-15-06_ede2472d34"
+    # "NormalizingAE/July_02_15-15-06_ede2472d34",
+    "VAE_GAN/July_20_23-46-36_8849b5bd57",
+    "3D_rec_gan/July_20_19-36-48_7ed486bdf5"
 ]
 
 if __name__ == "__main__":
@@ -22,19 +25,17 @@ if __name__ == "__main__":
         MODELS_TO_EVALUATE = [args.trial]
 
 
-    train_ds, test_ds = data_tools.load_dataset("shapenet", shuffle=False)
-    test_ds = data_tools.simulate_input(test_ds, 0, 0, 0)
-
-    _, test_ds_metadata = data_tools.load_shapenet_metadata(shuffle=False)
-    test_set_size = 0
-    for _ in test_ds_metadata:
-        test_set_size += 1
-
-    # test_ds = test_ds.skip(125).take(1)
 
     for trial_path in MODELS_TO_EVALUATE:
         print("Evaluating {}".format(trial_path))
         mr = ModelRunner(training=False, trial_path=trial_path)
+
+        _, test_ds = data_tools.load_dataset(mr.params['dataset'], shuffle=False, metadata_only=True)
+        for _ in test_ds_metadata:
+            test_set_size += 1
+        test_ds = data_tools.load_voxelgrids(test_ds)
+        test_ds = data_tools.preprocess_test_dataset(test_ds, mr.params)
+
         evaluation = {trial_path: model_evaluator.evaluate_model(mr.model, test_ds, test_set_size)}
         model_evaluator.save_evaluation(evaluation)
 
